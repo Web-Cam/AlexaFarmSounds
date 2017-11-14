a0a4ab0de9a2ebb30be7f3cf8365ac8bf4f422cc
Web-Cam <32422075+Web-Cam@users.noreply.github.com>
2017-11-13 21:26:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Alexa = require('alexa-sdk');
 
 var APP_ID = "amzn1.ask.skill.b8b4f1f0-a37e-4eba-9b29-a12993efa671"; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 var SKILL_NAME = 'Farm Sounds';
-var sheep = '<audio src="https://s3.amazonaws.com/alexafarmsounds/sheep.mp3"/>';
+var sheep = '<audio src="https://s3.amazonaws.com/alexafarmsounds/sheep.mp3"/>'; // Here are all the sounds, if you try to make your own remember to change rate to 16000 and 48kbs
 var dog = '<audio src="https://s3.amazonaws.com/alexafarmsounds/Dog.mp3"/>';
 var cat = '<audio src="https://s3.amazonaws.com/alexafarmsounds/Cat.mp3"/>';
 var duck = '<audio src="https://s3.amazonaws.com/alexafarmsounds/Duck.mp3"/>';
@@ -11,9 +11,9 @@ var rooster = '<audio src="https://s3.amazonaws.com/alexafarmsounds/Rooster2.mp3
 var cow = '<audio src="https://s3.amazonaws.com/alexafarmsounds/cow.mp3"/>';
 var horse = '<audio src="https://s3.amazonaws.com/alexafarmsounds/horse.mp3"/>';
 var chicken = '<audio src="https://s3.amazonaws.com/alexafarmsounds/chicken.mp3"/>';
-var last = "none";
-var tries = 0;
-var randomanimal = ['SayCow','SayCat','SaySheep','SayHorse','SayDuck','SayRooster','SayChicken'];
+var last = "none"; // Last means last function called
+var tries = 0; // This functions a bit wonky, will fix 
+var randomanimal = ['SayCow','SayCat','SaySheep','SayHorse','SayDuck','SayRooster','SayChicken']; //the array of functions that say the sounds
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
@@ -21,23 +21,23 @@ exports.handler = function(event, context, callback) {
     alexa.execute();
 };
 
-var handlers = {
+var handlers = { // Starting function once you say Start Farm Sounds
     'LaunchRequest': function () {
-        this.emit(':ask',"Welcome to farm sounds........ Say Start to begin");
+        this.emit(':ask',"Welcome to farm sounds........ Say Start to begin"); //need to say start becasue I rely on utternaces
 		last = "none";
     },
 
-'SayRooster': function () {
+'SayRooster': function () { // Begin the functions of different animal sounds
      this.emit(':ask',"what animal makes this sound?......"+`${rooster}`, "here's a hint .... they can wake you up");
 	 last = "rooster";
 	},
-	 'RoosterCheck': function () {
+	 'RoosterCheck': function () { //Checkers are called Via utternaces, while Say functions are called by random function at bottom
 	 if (last === "rooster"){
 	 this.emit(':ask',"Correct! The rooster goes COCK A DOOODLE DOOOOO ....... When your ready for the Next animal Say Next");
 	 tries = 0;
 	 }else{
 	tries +=1;
-	this.emit('Incorrect');
+	this.emit('Incorrect'); // Go to incorrect function
 	 }
 	},
 
@@ -153,7 +153,7 @@ var handlers = {
 	
 	'Incorrect': function () { // Should make this into a switch this got way carried away.
      if (last === "dog"){
-		if (tries <= 2){ // Tries really dont matter just a way to switch it between hints
+		if (tries <= 2){ // Tries really dont matter just a way to switch it between hints tries a bit messed up 
 				tries +=3 
 	 this.emit(':ask','Not quite ........ Here is a hint, the animal likes bones, and starts with D.......can you guess it now??');
 		}else{ 
@@ -165,7 +165,7 @@ var handlers = {
 	 this.emit(':ask','Not quite ........ Here is a hint, the animal is small, and starts with C .......can you guess it now??');
 		tries +=3 
 		}else{ 
-	 this.emit(':ask',"Almost there, These animals love to drink Milk");
+	 this.emit(':ask',"Almost there, These animals love to drink Milk");//Alexa says milk wierd, maybe change?
 	 }
 	}
 	
